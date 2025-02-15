11bc77c39feed7286ed56f8f4e9268d6690413d2
jiholee1210 <jiholee1210@gmail.com>
2025-02-15 23:34:16 +0900

    feat: 플레이어 점프 구현

--- Fishing/Assets/Script/PlayerMovement.cs
+++ Fishing/Assets/Script/PlayerMovement.cs
@@ -4,21 +4,23 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
 
     float inputValueX;
     float inputValueZ;
     float input;
     float currentSpeedX;
     float currentSpeedZ;
+    bool isGrounded = true;
 
     public Vector3 inputVec;
 
-    private Rigidbody rigidbody;
+    private Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -33,12 +35,22 @@ public class PlayerMovement : MonoBehaviour
         currentSpeedZ = inputValueZ * speed;
 
         inputVec = new Vector3(inputValueX, 0, inputValueZ);
-        Vector3 velocity = transform.TransformDirection(new Vector3(currentSpeedX, rigidbody.linearVelocity.y, currentSpeedZ));
-        rigidbody.linearVelocity = velocity;
+        Vector3 velocity = transform.TransformDirection(new Vector3(currentSpeedX, rb.linearVelocity.y, currentSpeedZ));
+        rb.linearVelocity = velocity;
     }
 
     public void OnMove(InputValue value) {
         inputValueX = value.Get<Vector2>().x;
         inputValueZ = value.Get<Vector2>().y;
+    }
+
+    public void OnJump(InputValue value) {
+        if(value.isPressed && isGrounded) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
